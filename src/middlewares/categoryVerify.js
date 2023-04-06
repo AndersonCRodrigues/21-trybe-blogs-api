41d4fb03757468a8c3f055fce90b971f9d80ce65
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-06 17:23:52 -0300

    [feat] add category name middleware

--- src/middlewares/categoryVerify.js
+++ src/middlewares/categoryVerify.js
@@ -1,3 +1,13 @@
+const categorySchema = require('../schemas/category.schema');
+const errorGenerate = require('../utils/errorGenerate');
+
 const checkCategoryName = (req, res, next) => {
-  
+  const { name } = req.body;
+  const { error } = categorySchema.validate({ name });
+
+  if (error) throw errorGenerate(400, error.message);
+
+  next();
 };
+
+module.exports = { checkCategoryName };

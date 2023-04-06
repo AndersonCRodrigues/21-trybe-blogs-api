63517d2d13db156eadeb1e83ea924a4468788452
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-06 19:08:26 -0300

    [feat] add post create controller

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,13 @@
+const postService = require('../services/post.service');
+
+const create = async (req, res, next) => {
+  try {
+    const token = req.headers.autrhorization;
+    const data = await postService.create(req.body, token);
+    res.status(201).json(data);
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = { create };

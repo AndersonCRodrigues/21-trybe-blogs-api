a81bb24a805550ae7593bcd095c887512c0b198d
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-06 18:18:33 -0300

    [feat] add post joi schema

---
+++ src/schemas/post.schema.js
@@ -0,0 +1,13 @@
+const Joi = require('joi');
+
+const categorySchema = Joi.object({
+  title: Joi.string().required().label('name'),
+  content: Joi.string().required().label('name'),
+  categoryIds: Joi.array().min(1).required().label('name'),
+}).messages({
+  'any.required': 'Some required fields are missing',
+  'array.min': 'Some required fields are missing',
+  'any.empty': 'Some required fields are missing',
+});
+
+module.exports = categorySchema;

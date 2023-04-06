c8d5c5decd1964bff3843377ad4df83f222e28bc
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-06 17:32:03 -0300

    [feat] add category create controller

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,9 @@
+const categoryService = require('../services/category.service');
+
+const create = async (req, res) => {
+  const { name } = req.body;
+  const data = await categoryService.create(name);
+  res.status(201).json(data);
+};
+
+module.exports = { create };

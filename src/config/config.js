5571bb4e5c9158445f9aa67252907b14270fd03d
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-04-14 16:06:32 -0300

    add config para postgres

--- src/config/config.js
+++ src/config/config.js
@@ -1,21 +1,27 @@
-const environment = process.env.NODE_ENV || 'test';
+require('dotenv').config();
+
+/* const environment = process.env.NODE_ENV || 'test';
 
 const suffix = {
   dev: '-dev',
   development: '-dev',
   test: '-test',
-};
+}; */
 
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
+  url: process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
+  database: process.env.MYSQL_DB_NAME || 'blogs_api',
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
-  dialect: 'mysql',
+  dialect: 'postgres',
   dialectOptions: {
+    statementTimeout: 10,
     timezone: 'Z',
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: true, // This line will fix new error
+    },
   },
   logging: process.env.DEBUG !== 'false',
 };
